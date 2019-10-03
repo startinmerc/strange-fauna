3ca450a16a173d10b77f004669552782122b68fb
STM <startinmerc@gmail.com>
2019-10-03 16:00:33 +0200

    add menu links to Landing

--- src/landing.js
+++ src/landing.js
@@ -14,11 +14,11 @@ class Landing extends Component {
 				Chillwave PBR&B sustainable pitchfork poke kitsch. Literally biodiesel direct trade kinfolk mixtape tbh snackwave. Polaroid beard echo park fixie hella meh hell of. Intelligentsia chartreuse cronut hammock, green juice vinyl ramps taxidermy artisan meditation tilde chia ennui hexagon. La croix small batch mustache, aesthetic +1 jean shorts listicle authentic copper mug church-key unicorn ugh. Brunch tilde bushwick, fashion axe adaptogen poke pinterest chambray deep v knausgaard paleo neutra wolf shabby chic.
 			</p>
 			*/}
-			<div className="color" id="color-1"></div>
-			<div className="color" id="color-2"></div>
-			<div className="color" id="color-3"></div>
-			<div className="color" id="color-4"></div>
-			<div className="color" id="color-5"></div>
+			<div className="color" id="color-1"><a href="#">Mushrooms</a></div>
+			<div className="color" id="color-2"><a href="#">Berries</a></div>
+			<div className="color" id="color-3"><a href="#">Flowers</a></div>
+			<div className="color" id="color-4"><a href="#">Reductions</a></div>
+			<div className="color" id="color-5"><a href="#">About Us</a></div>
 			</main>
 		);
 	}
