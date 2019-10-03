ec769953f4b3dc0f6774ec0cc8e5aca843829600
STM <startinmerc@gmail.com>
2019-10-03 15:49:25 +0200

    main to Landing component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './header';
 import Footer from './footer';
+import Landing from './landing';
 
 class Product extends Component {
 	render(){
@@ -25,21 +26,7 @@ class App extends Component {
 		return(
 			<div>
 				<Header />
-				<main>
-				{pList}
-				{/*
-				<h1 style={{textAlign: 'center', fontSize: '10rem', margin: 0}}>Strange Fauna</h1>
-				<p>
-					Chillwave PBR&B sustainable pitchfork poke kitsch. Literally biodiesel direct trade kinfolk mixtape tbh snackwave. Polaroid beard echo park fixie hella meh hell of. Intelligentsia chartreuse cronut hammock, green juice vinyl ramps taxidermy artisan meditation tilde chia ennui hexagon. La croix small batch mustache, aesthetic +1 jean shorts listicle authentic copper mug church-key unicorn ugh. Brunch tilde bushwick, fashion axe adaptogen poke pinterest chambray deep v knausgaard paleo neutra wolf shabby chic.
-				</p>
-				*/}
-				<div className="color" id="color-1"></div>
-				<div className="color" id="color-2"></div>
-				<div className="color" id="color-3"></div>
-				<div className="color" id="color-4"></div>
-				<div className="color" id="color-5"></div>
-				
-				</main>
+				<Landing />
 				<Footer />
 			</div>
 		)
