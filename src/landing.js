09fc801913db876275e8f325890fa68f55c2f205
STM <startinmerc@gmail.com>
2019-10-16 17:49:48 +0200

    add options & submenu from codepen dev file (link in readme)

--- src/landing.js
+++ src/landing.js
@@ -1,16 +1,58 @@
 import React, {Component} from 'react';
 import './landing.css';
 
+
+class Submenu extends Component {
+	render(){
+		return(
+			<div className="submenu" style={{backgroundColor: `${this.props.color}`}}>
+				<div className="submenu-section">
+					<img src="https://picsum.photos/200" alt="" />
+					<h5>Submenu Option</h5>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+				</div>
+				<div className="submenu-section">
+					<img src="https://picsum.photos/200" alt="" />
+					<h5>Submenu Option</h5>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+				</div>
+				<div className="submenu-section">
+					<img src="https://picsum.photos/200" alt="" />
+					<h5>Submenu Option</h5>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+				</div>
+			</div>
+		)
+	}
+}
+
 class Landing extends Component {
+	constructor(props){
+		super(props)
+		this.handleEnter = this.handleEnter.bind(this);
+		this.handleLeave = this.handleLeave.bind(this);
+	}
+
+	handleEnter(e){
+		document.querySelector('.submenu').style.transform = 'scaleY(1)';
+	}
+
+	handleLeave(e){
+		document.querySelector('.submenu').style.transform = 'scaleY(0)';
+	}
+
 	render(){
 		return(
 			<main>
-			<h1 className="display landing-header">Strange Fauna</h1>
-			<div className="color" id="color-1"><a href="#">Mushrooms</a></div>
-			<div className="color" id="color-2"><a href="#">Berries</a></div>
-			<div className="color" id="color-3"><a href="#">Flowers</a></div>
-			<div className="color" id="color-4"><a href="#">Reductions</a></div>
-			<div className="color" id="color-5"><a href="#">About Us</a></div>
+				<h1 className="display landing-header">Strange Fauna</h1>
+				<div className="options">
+					<li className="landing-option" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>Mushrooms</li>
+					<li className="landing-option" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>Berries</li>
+					<li className="landing-option" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>Flowers</li>
+					<li className="landing-option" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>Reductions</li>
+					<li className="landing-option" onMouseEnter={this.handleEnter} onMouseLeave={this.handleLeave}>About Us</li>
+					<Submenu />
+				</div>
 			</main>
 		);
 	}
