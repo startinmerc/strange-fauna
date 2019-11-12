565e20c07b420828d0ed45c963dc6f72fd97f724
STM <startinmerc@gmail.com>
2019-11-12 15:15:14 +0100

    remove unused JS

--- src/App.js
+++ src/App.js
@@ -3,28 +3,11 @@ import './App.css';
 import Header from './header';
 import Footer from './footer';
 import Landing from './landing';
-
-class Product extends Component {
-	render(){
-		return (
-		<div className="product-card">
-			<h4>Product Title</h4>
-			<p>Chillwave PBR&B sustainable pitchfork poke kitsch.</p>
-		</div>
-		)
-	}
-}
-
-
 import Mushrooms from './Mushrooms';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
 	render(){
-		const pList = [];
-		for (var i = 0; i <= 6; i++) {
-			pList.push(<Product />)
-		}
 		return(
 			<div>
 					<Header />
