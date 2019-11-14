1ddf8b12ef0272f62101d8258d87a42ba8e6d6b3
STM <startinmerc@gmail.com>
2019-11-14 17:33:48 +0100

    add mini-cart

--- src/Components/Partials/Header.js
+++ src/Components/Partials/Header.js
@@ -2,15 +2,43 @@ import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
+function MiniMenu(){
+	return (
+		<ul className="mini-cart">
+			<li>MiniMenu</li>
+			<li>MiniMenu</li>
+			<li>MiniMenu</li>
+		</ul>
+	)
+}
+
+function Wishlist(){
+	return (
+		<div className="header-container" id="header-wish">
+			<button className="header-button">Wishlist (0)</button>
+			<MiniMenu />
+		</div>	
+	)
+}
+
+function Cart(){
+	return (
+		<div className="header-container" id="header-cart">
+			<button className="header-button">Cart (0): $</button>
+			<MiniMenu />
+		</div>	
+	)
+}
+
 class Header extends Component {
 	render(){
 		return(
 			<header className="header">
 				<Link to="/" style={{textDecoration: 'none'}}><h2 className="header-title">Strange Fauna</h2></Link>
-				<nav className="header-links">
-					<li className="header-li">Wishlist</li>
-					<li className="header-li">Cart</li>
-				</nav>
+				<div className="header-links">
+					<Wishlist />
+					<Cart />
+				</div>
 			</header>
 		);
 	}
