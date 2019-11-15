705d1ecb0dafd3a6282232a09b274410bfada3a4
STM <startinmerc@gmail.com>
2019-11-15 12:07:26 +0100

    nav component in header; restyle header to fit

--- src/Components/Partials/Header.js
+++ src/Components/Partials/Header.js
@@ -30,15 +30,36 @@ function Cart(){
 	)
 }
 
+function Nav(){
+		const colors = [
+			{title: 'Mushrooms', ref: 'mushroom', color: 'var(--mushroom)'},
+			{title: 'Berries', ref: 'berry', color: 'var(--berry)'},
+			{title: 'Flowers', ref: 'flower', color: 'var(--flower)'},
+			{title: 'Reductions', ref: 'reduction', color: 'var(--reduction)'},
+			{title: 'About Us', ref: 'about', color: 'var(--primary)'}
+		];
+		const ops = colors.map((option,index)=>(
+			<li key={'option-'+index} className="nav-link" style={{backgroundColor: option.color}}>
+				<Link to={
+					(option.ref==='about') ? '/about' : `/products/${option.title}`
+				}>{option.title}</Link>
+			</li>
+		 ));
+		return (<div className="nav-link-container">{ops}</div>);
+}
+
 class Header extends Component {
 	render(){
 		return(
 			<header className="header">
-				<Link to="/" style={{textDecoration: 'none'}}><h2 className="header-title">Strange Fauna</h2></Link>
-				<div className="header-links">
-					<Wishlist />
-					<Cart />
+				<div className="header-main">
+					<Link to="/" style={{textDecoration: 'none'}}><h2 className="header-title">Strange Fauna</h2></Link>
+					<div className="header-carts">
+						<Wishlist />
+						<Cart />
+					</div>
 				</div>
+				<Nav />
 			</header>
 		);
 	}
