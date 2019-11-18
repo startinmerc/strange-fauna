0ecb6bd46b1bccfc9063dde24db183deae45d116
STM <startinmerc@gmail.com>
2019-11-18 19:08:59 +0100

    correct link in SectionItem render

--- src/Components/Nav/NavSectionItem.js
+++ src/Components/Nav/NavSectionItem.js
@@ -6,7 +6,7 @@ class SectionItem extends Component {
 		return (
 			<div className="nav-section-submenu-section">
 				<img src={this.props.item.photos[0]} alt="" />
-				<Link to={`/products/${this.props.item.type}`}><h5>{this.props.item.name}</h5></Link>
+				<Link to={`/products/${this.props.item.id}`}><h5>{this.props.item.name}</h5></Link>
 				<p>{this.props.item.description}</p>
 			</div>
 		)
