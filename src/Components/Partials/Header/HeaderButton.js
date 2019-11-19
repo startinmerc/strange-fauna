7b29580aa3487c71cf3f3a6aa1014d2a63ca95f2
STM <startinmerc@gmail.com>
2019-11-19 11:47:15 +0100

    dynamically render headerbuttons

--- src/Components/Partials/Header/HeaderButton.js
+++ src/Components/Partials/Header/HeaderButton.js
@@ -9,6 +9,12 @@ class HeaderButton extends Component {
 		this.props.content.forEach((id)=>(
 		 cartContent.push(seeds.find((item)=>(item.id === id)))
 		));
+
+		let text = (this.props.type==="wish") ? 
+			`Wishlist (${this.props.content.length})`
+			 :
+		  `Cart (${this.props.content.length}): $${cartContent.reduce((a,v)=>(a+v.price),0)}`;
+
 		return (
 			<div className="header-container" id={`header-${this.props.type}`}>
 				<button className="header-button">{text}</button>
