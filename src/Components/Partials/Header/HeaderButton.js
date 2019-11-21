9f901a8fa3ac4d6ea01a07587db5601df33a8462
STM <startinmerc@gmail.com>
2019-11-21 17:45:22 +0100

    provisional remove function in headerButton with minicart de-factored

--- src/Components/Partials/Header/HeaderButton.js
+++ src/Components/Partials/Header/HeaderButton.js
@@ -1,24 +1,53 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
+import { removeWish } from '../../../actionCreators'
+// import MiniCart from './MiniCart';
 import seeds from '../../../seeds';
 
 class HeaderButton extends Component {
+	constructor(props){
+		super(props);
+		this.handleAdd = this.handleAdd.bind(this);
+	}
+
+	handleAdd(val){
+		this.props.addTodo(val);
+	}
+
+	removeWish(id){
+		this.props.removeWish(id);
+	}
+
 	render(){
-
-		let cartContent = [];
-		this.props.content.forEach((id)=>(
-		 cartContent.push(seeds.find((item)=>(item.id === id)))
+		let cartContent = [], text = '';
+		if(this.props.type==="wish"){
+			this.props.wish.forEach((id)=>(
+				cartContent.push(seeds.find((item)=>(item.id === id)))
+			));
+			text = `Wishlist (${this.props.wish.length})`
+		} else {
+			this.props.cart.forEach((id)=>(
+				cartContent.push(seeds.find((item)=>(item.id === id)))
+			));
+			text = `Cart (${this.props.cart.length}): $${cartContent.reduce((a,v)=>(a+v.price),0)}`;
+		};
+		let list = cartContent.map(item => (
+			<li key={`mini${item.type}-${item.id}`} className="mini-cart-item" 
+			 style={{backgroundColor: `var(--${item.type})`}}>
+				<div className="mini-cart-item-text">
+					<p>{item.name}</p>
+					<p>${item.price}</p>
+					<p><button onClick={this.removeWish.bind(this, item.id)}>Remove</button></p>
+				</div>
+				<img className="mini-cart-item-image" src={item.photos[0]} alt="" />
+			</li>
 		));
-
-		let text = (this.props.type==="wish") ? 
-			`Wishlist (${this.props.content.length})`
-			 :
-		  `Cart (${this.props.content.length}): $${cartContent.reduce((a,v)=>(a+v.price),0)}`;
-
 		return (
 			<div className="header-container" id={`header-${this.props.type}`}>
 				<button className="header-button">{text}</button>
-				<MiniCart content={cartContent}/>
+				<ul className="mini-cart">
+					{list}
+				</ul>
 			</div>
 		);
 	}
