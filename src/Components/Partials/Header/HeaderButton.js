b40b9092afecb17fba6f7a9a6177275feb8a3217
STM <startinmerc@gmail.com>
2019-11-21 18:25:23 +0100

    tidy HeaderButton render, conditional rendering for wish/cart

--- src/Components/Partials/Header/HeaderButton.js
+++ src/Components/Partials/Header/HeaderButton.js
@@ -1,39 +1,39 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
-import { removeWish } from '../../../actionCreators';
+import { removeWish, removeCart } from '../../../actionCreators';
 import MiniCartItem from './MiniCartItem';
 import seeds from '../../../seeds';
 
 class HeaderButton extends Component {
-	constructor(props){
-		super(props);
-		this.handleAdd = this.handleAdd.bind(this);
-	}
-
-	handleAdd(val){
-		this.props.addTodo(val);
-	}
 
 	removeWish(id){
 		this.props.removeWish(id);
 	}
 
+	removeCart(id){
+		this.props.removeCart(id);
+	}
+
 	render(){
-		let cartContent = [], text = '';
+		let cartContent = [], text = '', list = [];
 		if(this.props.type==="wish"){
 			this.props.wish.forEach((id)=>(
 				cartContent.push(seeds.find((item)=>(item.id === id)))
 			));
-			text = `Wishlist (${this.props.wish.length})`
+			text = `Wishlist (${this.props.wish.length})`;
+			list = cartContent.map(item => (
+				<MiniCartItem item={item} remove={this.removeWish.bind(this, item.id)}/>
+			));
 		} else {
 			this.props.cart.forEach((id)=>(
 				cartContent.push(seeds.find((item)=>(item.id === id)))
 			));
 			text = `Cart (${this.props.cart.length}): $${cartContent.reduce((a,v)=>(a+v.price),0)}`;
+			list = cartContent.map(item => (
+				<MiniCartItem item={item} remove={this.removeCart.bind(this, item.id)}/>
+			));
 		};
-		let list = cartContent.map(item => (
-			<MiniCartItem item={item} removeWish={this.removeWish.bind(this, item.id)}/>
-		));
+
 		return (
 			<div className="header-container" id={`header-${this.props.type}`}>
 				<button className="header-button">{text}</button>
@@ -52,4 +52,4 @@ function mapStateToProps(reduxState) {
 	};
 }
 
-export default connect(mapStateToProps, { removeWish })(HeaderButton);
+export default connect(mapStateToProps, { removeWish, removeCart })(HeaderButton);
--- src/Components/Partials/Header/MiniCartItem.js
+++ src/Components/Partials/Header/MiniCartItem.js
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
 
-const MiniCartItem = ({item, removeWish})=>(
+const MiniCartItem = ({item, remove})=>(
 	<li key={`mini${item.type}-${item.id}`} className="mini-cart-item" 
 	 style={{backgroundColor: `var(--${item.type})`}}>
 		<div className="mini-cart-item-text">
 			<p>{item.name}</p>
 			<p>${item.price}</p>
-			<p><button onClick={removeWish}>Remove</button></p>
+			<p><button onClick={remove}>Remove</button></p>
 		</div>
 		<img className="mini-cart-item-image" src={item.photos[0]} alt="" />
 	</li>
