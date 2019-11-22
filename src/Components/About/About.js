00dc14d2898814529722fa8d1a32321518520b4e
STM <startinmerc@gmail.com>
2019-11-22 15:04:36 +0100

    add ipsum to About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -27,11 +27,22 @@ function About() {
 				<Returns />
 			</Route>
 			<Route path={`${match.url}/`}>
-				<h1>About Us</h1>
-				<h3><Link to="/about/faq">FAQ</Link></h3>
-				<h3><Link to="/about/delivery">Delivery</Link></h3>
-				<h3><Link to="/about/returns">Returns</Link></h3>
-				<h3><Link to="/about/disclaimer">Disclaimer</Link></h3>
+				<main>
+					<h1>About Us</h1>
+					<p>
+						Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.
+					</p>
+					<p>
+						Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+					</p>
+					<p>
+						Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach avocado daikon napa cabbage asparagus winter purslane kale. Celery potato scallion desert raisin horseradish spinach carrot soko. Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke.
+					</p>
+					<h3><Link to="/about/faq">FAQ</Link></h3>
+					<h3><Link to="/about/delivery">Delivery</Link></h3>
+					<h3><Link to="/about/returns">Returns</Link></h3>
+					<h3><Link to="/about/disclaimer">Disclaimer</Link></h3>
+				</main>
 			</Route>
 		</Switch>
 	);
