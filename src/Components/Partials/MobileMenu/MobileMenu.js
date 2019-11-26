90f1bd27e89af0362426f8cdce0b8187c5bbf108
STM <startinmerc@gmail.com>
2019-11-26 17:46:45 +0100

    add dropup menu to MobileMenu

--- src/Components/Partials/MobileMenu/MobileMenu.js
+++ src/Components/Partials/MobileMenu/MobileMenu.js
@@ -4,10 +4,18 @@ import { connect } from "react-redux";
 import Star from '../../SVGs/Star';
 import ShoppingCart from '../../SVGs/ShoppingCart';
 import Menu from '../../SVGs/Menu';
+import categories from '../../../assets/categories';
 import './MobileMenu.css';
 
 class MobileMenu extends Component {
 	render(){
+		const sections = categories.map((section,index)=>(
+			<li key={'nav-section-'+index} className="dropup" style={{background: section.color}}>
+				<Link to={
+					(section.title!=='About Us') ? `/products/${section.title}` : '/about'
+				}>{section.title}</Link>
+			</li>
+		));
 		return(
 			<div id="mobile-menu">
 				<Link to="/wishlist" className={`header-button ${(this.props.wish.length > 0) ? null : 'empty'}`}>
@@ -18,7 +26,10 @@ class MobileMenu extends Component {
 					<ShoppingCart size={'2rem'}/>
 					<div className="mobile-menu-quantity">({this.props.cart.length})</div>
 				</Link>
-				<Menu size={'2rem'}/>
+				<button onClick={()=>{document.querySelector('.mobile-menu-dropup').classList.toggle('expanded')}}><Menu size={'2rem'}/></button>
+				<div className="mobile-menu-dropup">
+					{sections}
+				</div>
 			</div>
 		);
 	}
