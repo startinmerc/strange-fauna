cf5dff99ff69f481858a6c40bf4266c1b7f4eff1
STM <startinmerc@gmail.com>
2019-11-26 17:46:08 +0100

    tidy SVG components

--- src/Components/SVGs/ShoppingCart.js
+++ src/Components/SVGs/ShoppingCart.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ShoppingCart = ({size=48, color="#000000"}) => (
+
 	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
 	 viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="1"
 	 strokeLinecap="butt" strokeLinejoin="arcs" className="shopping-cart">
--- src/Components/SVGs/Star.js
+++ src/Components/SVGs/Star.js
@@ -3,7 +3,7 @@ import React from "react";
 const Star = ({size=48, color="#000000"}) => (
 
 	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
-	 viewBox="0 0 24 24" fill="#FFF" stroke={color} strokeWidth="1"
+	 viewBox="0 0 24 24" fill="var(--white)" stroke={color} strokeWidth="1"
 	 strokeLinecap="butt" strokeLinejoin="arcs" className="star">
 		<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
 	</svg>
