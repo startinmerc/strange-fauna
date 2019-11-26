df5bf289f62d1c3cdb52bf7b463da48dbba586a1
STM <startinmerc@gmail.com>
2019-11-26 17:10:56 +0100

    MobileMenu styling

--- src/Components/Partials/MobileMenu/MobileMenu.js
+++ src/Components/Partials/MobileMenu/MobileMenu.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
 import Star from '../../SVGs/Star';
 import ShoppingCart from '../../SVGs/ShoppingCart';
 import Menu from '../../SVGs/Menu';
@@ -9,13 +10,13 @@ class MobileMenu extends Component {
 	render(){
 		return(
 			<div id="mobile-menu">
-				<Link to="/wishlist" className="header-button empty">
+				<Link to="/wishlist" className={`header-button ${(this.props.wish.length > 0) ? null : 'empty'}`}>
 					<Star size={'2rem'}/>
-					<div className="mobile-menu-quantity">(1)</div>
+					<div className="mobile-menu-quantity">({this.props.wish.length})</div>
 				</Link>
-				<Link to="/cart" className="header-button empty">
+				<Link to="/cart" className={`header-button ${(this.props.cart.length > 0) ? null : 'empty'}`}>
 					<ShoppingCart size={'2rem'}/>
-					<div className="mobile-menu-quantity">(1)</div>
+					<div className="mobile-menu-quantity">({this.props.cart.length})</div>
 				</Link>
 				<Menu size={'2rem'}/>
 			</div>
@@ -23,4 +24,11 @@ class MobileMenu extends Component {
 	}
 }
 
-export default MobileMenu;
+function mapStateToProps(reduxState) {
+	return {
+		wish: reduxState.wish,
+		cart: reduxState.cart
+	};
+}
+
+export default connect(mapStateToProps)(MobileMenu);
