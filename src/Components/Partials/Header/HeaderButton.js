42080e82fbb4465c288a3cffdc3336d2903782be
STM <startinmerc@gmail.com>
2019-11-26 15:01:31 +0100

    add SVGs to HeaderButton & restyle

--- src/Components/Partials/Header/HeaderButton.js
+++ src/Components/Partials/Header/HeaderButton.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import { connect } from "react-redux";
 import { removeWish, removeCart } from '../../../actionCreators';
 import { Link } from "react-router-dom";
+import Star from '../../SVGs/Star';
+import ShoppingCart from '../../SVGs/ShoppingCart';
 import MiniCartItem from './MiniCartItem';
 import seeds from '../../../seeds';
 
@@ -22,14 +24,18 @@ class HeaderButton extends Component {
 				let item = (seeds.find((item)=>(item.id === id)));
 				list.push(<MiniCartItem key={`mini${item.type}-${item.id}`} item={item} remove={this.removeWish.bind(this, item.id)}/>);
 			});
-			link = <Link to="/wishlist" className={`header-button ${(list.length > 0) ? null : 'empty'}`}>{`Wishlist (${list.length})`}</Link>
+			link = <Link to="/wishlist" className={`header-button ${(list.length > 0) ? null : 'empty'}`}>
+				<Star size={'1.5rem'}/>{`Wishlist (${list.length})`}
+			</Link>
 		} else {
 			this.props.cart.forEach((id)=>{
 				let item = (seeds.find((item)=>(item.id === id)));
 				total += item.price;
 				list.push(<MiniCartItem key={`mini${item.type}-${item.id}`} item={item} remove={this.removeCart.bind(this, item.id)}/>);
 			});
-			link = <Link to="/cart" className={`header-button ${(list.length > 0) ? null : 'empty'}`}>{`Cart (${list.length}): $${total}`}</Link>
+			link = <Link to="/cart" className={`header-button ${(list.length > 0) ? null : 'empty'}`}>
+				<ShoppingCart size={'1.5rem'}/>{`Cart (${list.length}): $${total}`}
+			</Link>
 		};
 		return (
 			<div className="header-container" id={`header-${this.props.type}`}>
