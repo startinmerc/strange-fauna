fda8e35a5bc93e9f97e9436fd78cb00958e8e310
STM <startinmerc@gmail.com>
2019-11-27 10:55:58 +0100

    dropup show/hide styling

--- src/Components/Partials/MobileMenu/MobileMenu.js
+++ src/Components/Partials/MobileMenu/MobileMenu.js
@@ -18,17 +18,19 @@ class MobileMenu extends Component {
 		));
 		return(
 			<div id="mobile-menu">
-				<Link to="/wishlist" className={`header-button ${(this.props.wish.length > 0) ? null : 'empty'}`}>
-					<Star size={'2rem'}/>
-					<div className="mobile-menu-quantity">({this.props.wish.length})</div>
-				</Link>
-				<Link to="/cart" className={`header-button ${(this.props.cart.length > 0) ? null : 'empty'}`}>
-					<ShoppingCart size={'2rem'}/>
-					<div className="mobile-menu-quantity">({this.props.cart.length})</div>
-				</Link>
-				<button onClick={()=>{document.querySelector('.mobile-menu-dropup').classList.toggle('expanded')}}><Menu size={'2rem'}/></button>
 				<div className="mobile-menu-dropup">
 					{sections}
+				</div>
+				<div className="mobile-menu-content">
+					<Link to="/wishlist" className={`header-button ${(this.props.wish.length > 0) ? null : 'empty'}`}>
+						<Star size={'2rem'}/>
+						<div className="mobile-menu-quantity">({this.props.wish.length})</div>
+					</Link>
+					<Link to="/cart" className={`header-button ${(this.props.cart.length > 0) ? null : 'empty'}`}>
+						<ShoppingCart size={'2rem'}/>
+						<div className="mobile-menu-quantity">({this.props.cart.length})</div>
+					</Link>
+					<button onClick={()=>{document.querySelector('.mobile-menu-dropup').classList.toggle('expanded')}}><Menu size={'2rem'}/></button>
 				</div>
 			</div>
 		);
