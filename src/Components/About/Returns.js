d38f90ddf2b57abdc4fc0ffb12854d437d873c28
STM <startinmerc@gmail.com>
2019-11-27 17:41:40 +0100

    Returns template & styling

--- src/Components/About/Returns.js
+++ src/Components/About/Returns.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 function Returns(){
 	return (
-		<main>
-			<h1>Returns</h1>
+		<main id='returns'>
+			<h1 className="header">Returns</h1>
+			<div className="boxes">
+				<div className="box box-text">
+					<h3>Return Options</h3>
+					<p>Stare out cat door then go back inside leave dead animals as gifts meow to be let in. Going to catch the red dot today going to catch the red dot today leave hair on owner's clothes yet hide when guests come over refuse to drink water except out of someone's glass.</p>
+					<h3>Sleep on dog bed, force dog to sleep on floor.</h3>
+					<p>Sit on human kitty poochy yet get video posted to internet for chasing red dot. Demand to be let outside at once, and expect owner to wait for me as i think about it meowzer.</p>
+				</div>
+				<div className="box box-image"></div>
+				<div className="box box-image"></div>
+				<div className="box box-text">
+					<h3>Absoltely no chance.</h3>
+					<p>Stare out cat door then go back inside leave dead animals as gifts meow to be let in. Going to catch the red dot today going to catch the red dot today leave hair on owner's clothes yet hide when guests come over refuse to drink water except out of someone's glass.</p>
+					<h3>Sleep on dog bed, force dog to sleep on floor.</h3>
+					<p>Sit on human kitty poochy yet get video posted to internet for chasing red dot. Demand to be let outside at once, and expect owner to wait for me as i think about it meowzer.</p>
+				</div>
+			</div>
 		</main>
 	)
 }
