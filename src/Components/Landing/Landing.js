a018f465d2c2ffcd516170e545ec27e4e16e9d1b
STM <startinmerc@gmail.com>
2019-11-29 17:54:30 +0100

    add keys to landing sections

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './Landing.css';
-import { Link } from 'react-router-dom';
 import seeds from '../../seeds';
 import LandingSection from './LandingSection';
 
 function Landing() {
-	const sections = seeds.landingSections.map((seed)=>(
-		<LandingSection content={seed} />
+	const sections = seeds.landingSections.map((seed,index)=>(
+		<LandingSection content={seed} key={`section-${index}`}/>
 	))
 	return(
 		<main id="landing">
