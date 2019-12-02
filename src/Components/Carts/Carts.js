6f0d88b96d318c8a258a346286816d82e4eff92f
STM <startinmerc@gmail.com>
2019-12-02 11:09:12 +0100

    move delivery options to seeds, add documentation to Cart

--- src/Components/Carts/Carts.js
+++ src/Components/Carts/Carts.js
@@ -4,6 +4,8 @@ import ProductCard from '../Products/ProductCard';
 import seeds from '../../seeds';
 import './Carts.css';
 
+// Returns cart, expects type prop: 0 = wishlist, 1 = shopping cart.
+
 class List extends Component {
 	constructor(props){
 		super(props);
@@ -18,37 +20,41 @@ class List extends Component {
 	}
 
 	render(){
+
 		const options = {
 			ids: [this.props.wish,this.props.cart],
-			text: ["Your Wishlist","Your Cart"],
+			header: ["Your Wishlist","Your Cart"],
 			class: ['wish-list','cart-list'],
 			subtotal: [false,true]
 		};
-		const deliveries = [
-			{name: 'premium', price: 50},
-			{name: 'standard', price: 20}
-		]
-		let list = [], total = 0;
+
+		// Compile list of items in cart/wish based on ids from Redux state
+		let itemList = [], total = 0;
 		options.ids[this.props.type].forEach((id)=>{
+			// Find item in seeds from id
 			let item = (seeds.products.find((item)=>(item.id === id)));
+			// Update subtotal
 			total += item.price;
-			list.push(<ProductCard detail={item} key={`prod-${item.id}`}
+			// Push ProductCard to itemList
+			itemList.push(<ProductCard detail={item} key={`prod-${item.id}`}
+				// includes cart type prop if needed
 			 type={(options.subtotal[this.props.type]) ? "cart" : null}/>);
 		});
+
 		return (
 			<main>
 				<div className="cart-header">
-					<h2>{options.text[this.props.type]}{(list.length === 0) ? ' is empty' : null}</h2>
+					<h2>{options.header[this.props.type]}{(itemList.length === 0) ? ' is empty' : null}</h2>
 				</div>
 				<div className={options.class[this.props.type]}>
-					{list}
+					{itemList}
 				</div>
 				{options.subtotal[this.props.type] ? 
 					<div style={{textAlign: 'right'}}>
 						<p className="cart-subtotal">Subtotal: ${total}</p>
 							Choose delivery option: 
 						<select value={this.state.delivery} onChange={this.handleChange}>
-							{deliveries.map(op => (<option value={op.price}>{op.name}</option>))}
+							{seeds.deliveries.map(op => (<option value={op.price}>{op.name}</option>))}
 						</select>
 						<p>Delivery: ${this.state.delivery}</p>
 						<h2>Total: ${total + Number(this.state.delivery)}</h2>
