f9873e08aacc4a46b721301f1f167a3f34f156e9
STM <startinmerc@gmail.com>
2019-12-03 11:24:08 +0100

    remove unused categories file

--- src/assets/categories.js
+++
@@ -1,9 +0,0 @@
-const categories = [
-	{title: 'Mushrooms', section: 'mushroom', color: 'var(--mushroom)'},
-	{title: 'Berries', section: 'berry', color: 'var(--berry)'},
-	{title: 'Flowers', section: 'flower', color: 'var(--flower)'},
-	{title: 'Reductions', section: 'reduction', color: 'var(--reduction)'},
-	{title: 'About Us', section: 'about', color: 'var(--primary)'}
-];
-
-export default categories;
