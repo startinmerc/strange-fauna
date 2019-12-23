d662bd8ca57d48a5ed2cff3f0d8073cb04869e1a
STM <startinmerc@gmail.com>
2019-12-23 13:24:32 +0000

    landingSection links

--- src/seeds.js
+++ src/seeds.js
@@ -32,7 +32,7 @@ addToLanding(
 	'Buy seven mushrooms get one free! Limited stock available.',
 	'https://images.unsplash.com/photo-1528518290605-1fcc8dcca204?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
 	'mushroom',
-	[{title: 'Shop Now', link: '/'},{title: 'Shop Later', link: '/'}]
+	[{title: 'Shop Now', link: '/products/mushrooms'},{title: 'Shop Later', link: '/products/mushrooms'}]
 )
 addToLanding(
 	true,
@@ -40,7 +40,7 @@ addToLanding(
 	"See what we did there? It's a pun. You like puns, don't you? Yeah you do.",
 	'https://images.unsplash.com/photo-1445197138520-6099f1c07aa0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
 	'berry',
-	[{title: 'Shop Now', link: '/'},{title: 'Shop Later', link: '/'}]
+	[{title: 'Shop Now', link: '/products/berries'},{title: 'Shop Later', link: '/products/berries'}]
 )
 addToLanding(
 	false,
@@ -48,7 +48,7 @@ addToLanding(
 	"Lovely flowers. Just don't eat them. Really, don't eat them.",
 	'https://images.unsplash.com/photo-1567748157439-651aca2ff064?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
 	'flower',
-	[{title: 'Shop Now', link: '/'},{title: 'Shop Later', link: '/'}]
+	[{title: 'Shop Now', link: '/products/flowers'},{title: 'Shop Later', link: '/products/flowers'}]
 )
 
 function addToLanding(dark,header,copy,image,type,links){
