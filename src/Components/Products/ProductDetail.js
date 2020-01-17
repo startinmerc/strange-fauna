2bc2a54b842ad1d04c132ae5118cd4bc9aabd137
STM <startinmerc@gmail.com>
2020-01-17 18:11:02 +0000

    clear error

--- src/Components/Products/ProductDetail.js
+++ src/Components/Products/ProductDetail.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddToCart from './AddToCart';
 import seeds, { categories } from '../../seeds';
 import { useParams,	Link } from "react-router-dom";
