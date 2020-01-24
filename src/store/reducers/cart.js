57bf1da0437bcfc7eec7f436c0dda61e6a1aae95
STM <startinmerc@gmail.com>
2020-01-24 18:17:38 +0000

    Add ID to cart reducers

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -7,9 +7,9 @@ const DEFAULT_STATE = {
 export default (state = DEFAULT_STATE, action) => {
 	switch(action.type){
 		case ADD_CART:
-			return {...state, cart: [...state.cart, action.id]};
+			return {...state, cart: [...state.cart, {id: action.id, qty: action.qty}]};
 		case REMOVE_CART:
-			let newCart = state.cart.filter(val => val !== action.id);
+			let newCart = state.cart.filter(val => val.id !== action.id);
 			return {...state, cart: newCart};
 		default:
 			return state;
