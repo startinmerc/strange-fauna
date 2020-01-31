599bd94f9c2b77fd948c8adf04e211fd163da78b
STM <startinmerc@gmail.com>
2020-01-31 17:22:57 +0000

    remove wish from Carts

--- src/Components/Carts/Carts.js
+++ src/Components/Carts/Carts.js
@@ -7,7 +7,7 @@ import { deliveries } from '../../seeds';
 import './Carts.css';
 import { getItems } from '../../middleware';
 
-// Expects type prop: 0 = wishlist, 1 = shopping cart.
+// No props expected
 // Returns cart element
 
 class List extends Component {
@@ -21,43 +21,33 @@ class List extends Component {
 	}
 
 	render(){
-
-		const options = {
-			ids: [this.props.wish,this.props.cart],
-			header: ["Your Wishlist","Your Cart"],
-			class: ['wish-list','cart-list'],
-			subtotal: [false,true]
-		};
-
-		document.title = options.header[this.props.type];
-
-		let items = getItems(options.ids[this.props.type]);
+		// Define header
+		const header = "Your Cart";
+		document.title = header;
+		// Get full item info
+		let items = getItems(this.props.cart);
 		
 		return (
 			<main>
 				<div className="cart-header">
-					<h2>{options.header[this.props.type]}{(items.itemList.length === 0) ? ' is empty' : null}</h2>
+					<h2>{header}{(items.itemList.length === 0) ? ' is empty' : null}</h2>
 				</div>
-				<div className={options.class[this.props.type]}>
-					{
-						items.itemList.map(item => (
-						<ProductCard detail={item} key={`prod-${item.id}`}
-						// includes cart type prop if needed
-						 type={(options.subtotal[this.props.type]) ? "cart" : null}/>))
-					}
+				<div className="cart-list">
+					{items.itemList.map(item => (
+						<ProductCard detail={item} key={`prod-${item.id}`} type="cart"/>
+					))}
 				</div>
-				{options.subtotal[this.props.type] ? 
-					<div style={{textAlign: 'right'}}>
-						<p className="cart-subtotal">Subtotal: ${items.total}</p>
-						<p>
-							Choose delivery option:
-							<select value={this.props.delivery} onChange={this.handleChange}>
-								{deliveries.map((op,i) => (<option key={`del-op-${i}`} value={op.price}>{op.name} - ${op.price}</option>))}
-							</select>
-						</p>
-						<h2>Total: ${items.total + Number(this.props.delivery)}</h2>
-						<Link to="/checkout">Proceed to Checkout</Link>
-					</div> : null}
+				<div style={{textAlign: 'right'}}>
+					<p className="cart-subtotal">Subtotal: ${items.total}</p>
+					<p>
+						Choose delivery option:
+						<select value={this.props.delivery} onChange={this.handleChange}>
+							{deliveries.map((op,i) => (<option key={`del-op-${i}`} value={op.price}>{op.name} - ${op.price}</option>))}
+						</select>
+					</p>
+					<h2>Total: ${items.total + Number(this.props.delivery)}</h2>
+					<Link to="/checkout">Proceed to Checkout</Link>
+				</div>
 			</main>
 		)
 	}
@@ -65,7 +55,6 @@ class List extends Component {
 
 function mapStateToProps(reduxState) {
 	return {
-		wish: reduxState.wish.wish,
 		cart: reduxState.cart.cart,
 		delivery: reduxState.other.delivery
 	};
