d767ffa70c58786f3847c37dd1aa4840aced061f
STM <startinmerc@gmail.com>
2020-02-05 17:38:44 +0100

    Footer BEM

--- src/Components/Partials/Footer/Footer.js
+++ src/Components/Partials/Footer/Footer.js
@@ -8,22 +8,22 @@ class Footer extends Component {
 	render(){
 		return(
 			<footer className="footer">
-				<ul className="footer-link-group">
-					<li className="footer-link"><Link to="/products/all">Our Products</Link></li>
-					<li className="footer-link-sub"><Link to="/products/mushrooms">Mushrooms</Link></li>
-					<li className="footer-link-sub"><Link to="/products/berries">Berries</Link></li>
-					<li className="footer-link-sub"><Link to="/products/flowers">Flowers</Link></li>
-					<li className="footer-link-sub"><Link to="/products/reductions">Reductions</Link></li>
+				<ul className="footer__link-group">
+					<li className="footer__link"><Link to="/products/all">Our Products</Link></li>
+					<li className="footer__link-sub"><Link to="/products/mushrooms">Mushrooms</Link></li>
+					<li className="footer__link-sub"><Link to="/products/berries">Berries</Link></li>
+					<li className="footer__link-sub"><Link to="/products/flowers">Flowers</Link></li>
+					<li className="footer__link-sub"><Link to="/products/reductions">Reductions</Link></li>
 				</ul>
-				<ul className="footer-link-group">
-					<li className="footer-link"><Link to="/about">About Us</Link></li>
-					<li className="footer-link-sub"><Link to="/about/faq">FAQ</Link></li>
-					<li className="footer-link-sub"><Link to="/about/delivery">Delivery</Link></li>
-					<li className="footer-link-sub"><Link to="/about/returns">Returns</Link></li>
-					<li className="footer-link-sub"><Link to="/about/disclaimer">Disclaimer</Link></li>
+				<ul className="footer__link-group">
+					<li className="footer__link"><Link to="/about">About Us</Link></li>
+					<li className="footer__link-sub"><Link to="/about/faq">FAQ</Link></li>
+					<li className="footer__link-sub"><Link to="/about/delivery">Delivery</Link></li>
+					<li className="footer__link-sub"><Link to="/about/returns">Returns</Link></li>
+					<li className="footer__link-sub"><Link to="/about/disclaimer">Disclaimer</Link></li>
 				</ul>
-				<p className="footer-external">
-					A <a className="footer-link" rel="noopener noreferrer"
+				<p className="footer__external">
+					A <a className="footer__link" rel="noopener noreferrer"
 					 target="_blank" href="https://www.strangeindustries.co.uk">
 						Strange Industries
 					</a> Site.
