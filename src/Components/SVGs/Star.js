b42b85327186f029bd8b5332a9387193955639cd
STM <startinmerc@gmail.com>
2020-02-06 13:41:17 +0100

    fix specificity on icons w/ .icon

--- src/Components/SVGs/Star.js
+++ src/Components/SVGs/Star.js
@@ -4,7 +4,7 @@ const Star = ({size=48, color="#000000"}) => (
 
 	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
 	 viewBox="0 0 24 24" fill="var(--white)" stroke={color} strokeWidth="1"
-	 strokeLinecap="butt" strokeLinejoin="arcs" className="star">
+	 strokeLinecap="butt" strokeLinejoin="arcs" className="icon star">
 		<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
 	</svg>
 
