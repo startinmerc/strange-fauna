346f4b0f58b2dfae5ab11760f117581b27901793
STM <startinmerc@gmail.com>
2020-02-08 16:35:07 +0100

    accept fill prop to Star

--- src/Components/SVGs/Star.js
+++ src/Components/SVGs/Star.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Star = ({size=48, color="#000000"}) => (
+const Star = ({size=48, color="var(--black)", fill="none"}) => (
 
 	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
-	 viewBox="0 0 24 24" fill="var(--white)" stroke={color} strokeWidth="1"
+	 viewBox="0 0 24 24" fill={fill} stroke={color} strokeWidth="1"
 	 strokeLinecap="butt" strokeLinejoin="arcs" className="icon star">
 		<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
 	</svg>
