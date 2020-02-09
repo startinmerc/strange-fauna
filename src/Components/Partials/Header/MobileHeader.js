d46832940378cc9f0020b47b92642195526fa867
STM <startinmerc@gmail.com>
2020-02-09 18:35:46 +0100

    clean up MobileHeader

--- src/Components/Partials/Header/MobileHeader.js
+++ src/Components/Partials/Header/MobileHeader.js
@@ -5,12 +5,10 @@ import { Link } from 'react-router-dom';
 
 function Header() {	
 	return(
-		<header id="mobile-header" style={{borderBottom: '5px solid var(--primary)'}}>
-			<div className="header__main-container">
-				<Link to="/" style={{textDecoration: 'none'}}>
-					<h2 className="header__title">Strange Flora</h2>
-				</Link>
-			</div>
+		<header id="mobile-header">
+			<Link to="/">
+				<h2 className="header__title">Strange Flora</h2>
+			</Link>
 		</header>
 	);
 }
