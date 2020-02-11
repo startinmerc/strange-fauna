6c1e35a24e42ca6e2029165a1ef469e1c4a04d8d
STM <startinmerc@gmail.com>
2020-02-11 17:17:42 +0100

    restyle FAQ to show/hide questions

--- src/Components/About/FAQ.js
+++ src/Components/About/FAQ.js
@@ -1,29 +1,48 @@
 import React from 'react';
 
-// Returns static about section
-
-function Question() {
-	return (
-		<li>
-			<h4>Question?</h4>
-			<p>
-				Answer!<br/>
-				Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
-			</p>
-		</li>
-	)
-}
-
-
 function FAQ(){
 	document.title = 'FAQ';
+
+	function handleClick(e){
+		e.target.parentNode.classList.toggle('expanded');
+	}
+
 	return (
 		<main id="faq">
 			<h1>FAQ</h1>
 			<ul className="faq-list">
-				<Question />
-				<Question />
-				<Question />
+				<li className="question">
+					<button className="question__header display"
+					 onClick={handleClick}>Question?</button>
+					<p className="question__answer">
+						Answer!<br/>
+						Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+					</p>
+				</li>
+				<li className="question">
+					<button className="question__header display"
+					 onClick={handleClick}>Question?</button>
+					<p className="question__answer">
+						Answer!<br/>
+						Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+					</p>
+				</li>
+				<li className="question">
+					<button className="question__header display"
+					 onClick={handleClick}>Question?</button>
+					<p className="question__answer">
+						Answer!<br/>
+						Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+					</p>
+				</li>
+				<li className="question">
+					<button className="question__header display"
+					 onClick={handleClick}>Question?</button>
+					<p className="question__answer">
+						Answer!<br/>
+						Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.
+					</p>
+				</li>
 			</ul>
 		</main>
 	)
