35b736424b381e8943e6ad59b4c69738212015f2
STM <startinmerc@gmail.com>
2020-02-21 15:26:09 +0100

    adaptive Star SVG header

--- src/Components/SVGs/Star.js
+++ src/Components/SVGs/Star.js
@@ -5,7 +5,7 @@ const Star = ({size=48, color="var(--black)", fill="none", strokeWidth="1"}) => 
 	<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
 	 viewBox="0 0 24 24" fill={fill} stroke={color} strokeWidth={strokeWidth} role="img"
 	 strokeLinecap="butt" strokeLinejoin="arcs" className="icon star">
-	 <title>Star Icon</title>
+	 <title>Star Icon ({fill === "none" ? "Unfilled" : "Filled"})</title>
 		<polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
 	</svg>
 
