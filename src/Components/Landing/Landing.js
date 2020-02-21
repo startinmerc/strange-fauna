4608c2ede948abe1ab38f81f80e0e64c8d33a00b
STM <startinmerc@gmail.com>
2020-02-21 16:28:51 +0100

    Cta tweaks

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -14,6 +14,7 @@ function Landing() {
 
 	function handleClick(){
 		// Not great, needs to be ref when I learn!
+		window.scrollTo(0,document.body.scrollHeight);
 		document.querySelector("#email").focus();
 	}
 
