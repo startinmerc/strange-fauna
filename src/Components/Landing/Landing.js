aa8a0cf0126ae5e8dacb8cf997e58040366779dc
STM <startinmerc@gmail.com>
2020-02-21 16:25:40 +0100

    add CTA section to landing page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -10,7 +10,21 @@ function Landing() {
 
 	const sections = seeds.landingSections.map((seed,index)=>(
 		<LandingSection content={seed} key={`section-${index}`}/>
-	))
+	));
+
+	function handleClick(){
+		// Not great, needs to be ref when I learn!
+		document.querySelector("#email").focus();
+	}
+
+	sections.splice(1,0,
+		<div className="cta" key="cta">
+			<button className="cta__button display" onClick={handleClick}>
+				Sign up to our newsletter!
+			</button>
+			<p className="cta__content">Sign up to get our latest products, top deals and inspiring stories straight to your inbox. Plus, get £5 off your first order over £50</p>
+		</div>
+	);
 
 	return(
 		<main id="landing">
