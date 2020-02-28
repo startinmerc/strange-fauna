bfa04dd0776b12cbec7db3a0d658cb7d3f816793
STM <startinmerc@gmail.com>
2020-02-28 11:34:41 +0100

    Lose link focus on click in navbar

--- src/Components/Nav/NavSection.js
+++ src/Components/Nav/NavSection.js
@@ -8,28 +8,33 @@ import ProductCard from '../Products/ProductCard'
 // Or list of about links
 
 class NavSection extends Component {
+
+	handleClick(e){
+		e.target.blur();
+	}
+
 	render(){
 		if(this.props.section==='about') {
 			return (
 				<li className="nav-section" style={{backgroundColor: `${this.props.color}`}}>
-				<Link to='/about' className={`nav-section__block-link nav--${this.props.section}`}>
-					{this.props.title}
-				</Link>
-				<div className="nav-section__submenu about-submenu" style={{backgroundColor: `${this.props.color}`}}>
-					<Link to='/about' className="about-submenu__link">About Us</Link>
-					<Link to='/about/faq' className="about-submenu__link">FAQ</Link>
-					<Link to='/about/delivery' className="about-submenu__link">Delivery</Link>
-					<Link to='/about/returns' className="about-submenu__link">Returns</Link>
-					<Link to='/about/disclaimer' className="about-submenu__link">Disclaimer</Link>
-				</div>
-			 </li>
+					<Link onClick={this.handleClick} to='/about' className={`nav-section__block-link nav--${this.props.section}`}>
+						{this.props.title}
+					</Link>
+					<div className="nav-section__submenu about-submenu" style={{backgroundColor: `${this.props.color}`}}>
+						<Link onClick={this.handleClick} to='/about' className="about-submenu__link">About Us</Link>
+						<Link onClick={this.handleClick} to='/about/faq' className="about-submenu__link">FAQ</Link>
+						<Link onClick={this.handleClick} to='/about/delivery' className="about-submenu__link">Delivery</Link>
+						<Link onClick={this.handleClick} to='/about/returns' className="about-submenu__link">Returns</Link>
+						<Link onClick={this.handleClick} to='/about/disclaimer' className="about-submenu__link">Disclaimer</Link>
+					</div>
+				 </li>
 			);
 		}
 		let link = `/products/${this.props.title}`;
 		let sectionItems = seeds.products.filter((seed)=>(seed.type === this.props.section));
 		return(
 			<li className="nav-section" style={{backgroundColor: `${this.props.color}`}}>
-				<Link to={link} className={`nav-section__block-link nav--${this.props.section}`}>{this.props.title}</Link>
+				<Link onClick={this.handleClick} to={link} className={`nav-section__block-link nav--${this.props.section}`}>{this.props.title}</Link>
 				<div className="nav-section__submenu" style={{backgroundColor: `${this.props.color}`}}>
 					<div className="nav-section__submenu-content">
 						<h2>{this.props.title}</h2>
@@ -37,7 +42,7 @@ class NavSection extends Component {
 							Secondary fermentation degrees plato units of bitterness, cask conditioned ale ibu real ale pint glass craft beer.
 							Krausen goblet grainy ibu brewhouse lagering finishing hops.
 						</p>
-						<Link to={link}>View All {this.props.title}</Link>
+						<Link onClick={this.handleClick} to={link}>View All {this.props.title}</Link>
 					</div>
 					<ProductCard type="nav" detail={sectionItems[Math.floor(Math.random()*sectionItems.length)]}/>
 					<ProductCard type="nav" detail={sectionItems[Math.floor(Math.random()*sectionItems.length)]}/>
