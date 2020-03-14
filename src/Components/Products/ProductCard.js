4cafcbf1a5e9aaae79fc5f2bb148980c53ad8250
STM <startinmerc@gmail.com>
2020-03-14 16:10:56 +0000

    ProductCard to functional component

--- src/Components/Products/ProductCard.js
+++ src/Components/Products/ProductCard.js
@@ -1,70 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
 import AddToCart from './AddToCart';
 import AddToWish from './AddToWish';
 import EditQuantity from './EditQuantity';
 import { Link } from "react-router-dom";
 import './ProductCard.css';
 
-// Expects 'detail' object to be passed in with product detail
+// Expects 'type' for renderinf and 'detail' object with product detail
 // Returns ProductCard based on 'type', with default
 
-class ProductCard extends Component {
-
-	render(){
-		switch(this.props.type){
-			case 'cart':
-				// Shopping Cart ProductCard
-				return (
-					<div className="product-card product-card--cart">
-						<img className="product-card__image" src={this.props.detail.photos[0]} alt={this.props.detail.name}/>
-							<p className="display">
-								<Link to={`/products/${this.props.detail.id}`}>
-									{this.props.detail.name}
-								</Link>
-							</p>
-							<AddToCart id={this.props.detail.id} stk={this.props.detail.stock}/>
-							<EditQuantity id={this.props.detail.id} qty={this.props.detail.qty} stk={this.props.detail.stock}/>
-							<p className="product-card__price">{this.props.detail.qty} x ${this.props.detail.price}</p>
-					</div>
-				);
-			case 'nav':
-				// Nav Section ProductCard
-				return (
-					<div className="product-card product-card--nav">
-						<AddToWish id={this.props.detail.id}/>
-						<Link to={`/products/${this.props.detail.id}`}>
-							<img className="product-card__image" src={this.props.detail.photos[0]} alt={this.props.detail.name}/>
-						</Link>
-						<div className="product-card__text">
-							<Link to={`/products/${this.props.detail.id}`}>
-								<h5>{this.props.detail.name}</h5>
+function ProductCard({type, detail, delay=0}) {
+	switch(type){
+		case 'cart':
+			// Shopping Cart ProductCard
+			return (
+				<div className="product-card product-card--cart">
+					<img className="product-card__image" src={detail.photos[0]} alt={detail.name}/>
+						<p className="display">
+							<Link to={`/products/${detail.id}`}>
+								{detail.name}
 							</Link>
-							 <h5>${this.props.detail.price}</h5>
-						</div>
-						<AddToCart id={this.props.detail.id} stk={this.props.detail.stock}/>
-					</div>
-				)
-			default:
-				// Default ProductCard
-				return (
-					<div className="product-card" style={{
-						backgroundColor: `var(--${this.props.detail.type})`,
-						animationDelay: `${this.props.delay*100}ms`
-					}}>
-						<AddToWish id={this.props.detail.id}/>
-						<Link to={`/products/${this.props.detail.id}`}>
-							<img className="product-card__image" src={this.props.detail.photos[0]} alt={this.props.detail.name}/>
+						</p>
+						<AddToCart id={detail.id} stk={detail.stock}/>
+						<EditQuantity id={detail.id} qty={detail.qty} stk={detail.stock}/>
+						<p className="product-card__price">{detail.qty} x ${detail.price}</p>
+				</div>
+			);
+		case 'nav':
+			// Nav Section ProductCard
+			return (
+				<div className="product-card product-card--nav">
+					<AddToWish id={detail.id}/>
+					<Link to={`/products/${detail.id}`}>
+						<img className="product-card__image" src={detail.photos[0]} alt={detail.name}/>
+					</Link>
+					<div className="product-card__text">
+						<Link to={`/products/${detail.id}`}>
+							<h5>{detail.name}</h5>
 						</Link>
-						<div className="product-card__text">
-							<Link to={`/products/${this.props.detail.id}`}>
-								<h5>{this.props.detail.name}</h5>
-							</Link>
-							 <h5>${this.props.detail.price}</h5>
-						</div>
-						<AddToCart id={this.props.detail.id} stk={this.props.detail.stock}/>
+						 <h5>${detail.price}</h5>
 					</div>
-				);
-		};
+					<AddToCart id={detail.id} stk={detail.stock}/>
+				</div>
+			);
+		default:
+			// Default ProductCard
+			return (
+				<div className="product-card" style={{
+					backgroundColor: `var(--${detail.type})`,
+					animationDelay: `${delay*100}ms`
+				}}>
+					<AddToWish id={detail.id}/>
+					<Link to={`/products/${detail.id}`}>
+						<img className="product-card__image" src={detail.photos[0]} alt={detail.name}/>
+					</Link>
+					<div className="product-card__text">
+						<Link to={`/products/${detail.id}`}>
+							<h5>{detail.name}</h5>
+						</Link>
+						 <h5>${detail.price}</h5>
+					</div>
+					<AddToCart id={detail.id} stk={detail.stock}/>
+				</div>
+			);
 	};
 };
 
