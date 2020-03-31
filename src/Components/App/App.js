559bdc8798dad8bfb26fcdd265b38a688e112402
STM <startinmerc@gmail.com>
2020-03-31 15:34:39 +0100

    App to functional component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -13,36 +13,31 @@ import { Route, Switch } from 'react-router-dom';
 import { isMobile } from '../../middleware';
 import { Helmet } from 'react-helmet';
 
-class App extends Component {
-
-	render(){
-		return(
-			<div id="container">
-				<Helmet>
-					<meta charSet="utf-8" />
-					<title>Strange Fauna</title>
-					<meta name="viewport" content="width=device-width, initial-scale=1" />
-					<meta name="theme-color" content="#000000" />
-					<meta	name="description" content="Vendor of commercial and private flowers and funghi"/>
-					<link rel="apple-touch-icon" href="logo192.png" />
-					<link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-					<title>Strange Flora</title>
-				</Helmet>
-				{isMobile() ? <MobileHeader /> : <Header />}
-				<Switch>
-					<Route path="/" component={Landing} exact />
-					<Route path="/products" component={Products} />
-					<Route path="/about" component={About} />
-					<Route path="/checkout" component={Checkout} />
-					<Route path='/wishlist' render={() => <ProductList type="wish" title="Your Wishlist"/>}/>
-					<Route path='/cart' render={() => <Cart />}/>
-					<Route component={Landing} />
-				</Switch>
-				<Footer />
-				{isMobile() ? <MobileMenu /> : null}
-			</div>
-		)
-	}
-}
+const App = ()=> (
+	<div id="container">
+		<Helmet>
+			<meta charSet="utf-8" />
+			<title>Strange Fauna</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta name="theme-color" content="#000000" />
+			<meta	name="description" content="Vendor of commercial and private flowers and funghi"/>
+			<link rel="apple-touch-icon" href="logo192.png" />
+			<link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+			<title>Strange Flora</title>
+		</Helmet>
+		{isMobile() ? <MobileHeader /> : <Header />}
+		<Switch>
+			<Route path="/" component={Landing} exact />
+			<Route path="/products" component={Products} />
+			<Route path="/about" component={About} />
+			<Route path="/checkout" component={Checkout} />
+			<Route path='/wishlist' render={() => <ProductList type="wish" title="Your Wishlist"/>}/>
+			<Route path='/cart' render={() => <Cart />}/>
+			<Route component={Landing} />
+		</Switch>
+		<Footer />
+		{isMobile() ? <MobileMenu /> : null}
+	</div>
+)
 
 export default App;
