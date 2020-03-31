7b9e14918680eafc11a7927237570d4ac3de126c
STM <startinmerc@gmail.com>
2020-03-31 16:10:14 +0100

    MobileMenu to functional component

--- src/Components/Partials/MobileMenu/MobileMenu.js
+++ src/Components/Partials/MobileMenu/MobileMenu.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
 import Star from '../../SVGs/Star';
@@ -11,58 +11,49 @@ import './MobileMenu.css';
 // Returns sticky footer mobile menu with cart & wishlist buttons,
 // Along with popup nav of sections
 
-class MobileMenu extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			expanded: false
-		}
-		this.showMenu = this.showMenu.bind(this);
-		this.closeMenu = this.closeMenu.bind(this);
+const MobileMenu = ({ wish, cart })=> {
+
+	const [expanded,setExpanded] = useState();
+
+	function showMenu(e){
+		setExpanded(true);
+		document.addEventListener('click', closeMenu);
 	}
 
-	showMenu(e){
-		this.setState({expanded: true}, () => {
-			document.addEventListener('click', this.closeMenu);
-		});
-	}
-
-	closeMenu(){
-		this.setState({expanded: false}, () => {
-			document.removeEventListener('click', this.closeMenu);
-		});
+	function closeMenu(){
+		setExpanded(false);
+		document.removeEventListener('click', closeMenu);
 	}
 	
-	render(){
-		const sections = categories.map((section,index)=>(
-			<li key={'nav-section-'+index} className={`dropup nav--${section.section}`} style={{background: section.color}}>
-				<Link to={(section.section !=='about') ? `/products/${section.title}` : '/about'}>
-					{section.title}
-				</Link>
-			</li>
-		));
-		return(
-			<div id="mobile-menu">
-				<div className={`mobile-menu__dropup ${(this.state.expanded) ? 'expanded' : null}`}>
-					<li id="dropup-background" aria-hidden="true"></li>
-					{sections}
-				</div>
-				<div className="mobile-menu__content">
-					<MobileHeaderButton 
-						headerIcon={<Star size={"2rem"} strokeWidth="1"/>}
-						id={'header-wish'}
-						url={'/wishlist'}
-						items={this.props.wish}/>
-					<MobileHeaderButton 
-						headerIcon={<ShoppingCart size={"2rem"} strokeWidth="1"/>}
-						id={'header-cart'}
-						url={'/cart'}
-						items={this.props.cart}/>
-					<Hamburger size={'2rem'} handleClick={this.showMenu} expanded={this.state.expanded}/>
-				</div>
+	const sections = categories.map((section,index)=>(
+		<li key={'nav-section-'+index} className={`dropup nav--${section.section}`} style={{background: section.color}}>
+			<Link to={(section.section !=='about') ? `/products/${section.title}` : '/about'}>
+				{section.title}
+			</Link>
+		</li>
+	));
+
+	return(
+		<div id="mobile-menu">
+			<div className={`mobile-menu__dropup ${expanded && 'expanded'}`}>
+				<li id="dropup-background" aria-hidden="true"></li>
+				{sections}
 			</div>
-		);
-	}
+			<div className="mobile-menu__content">
+				<MobileHeaderButton 
+					headerIcon={<Star size={"2rem"} strokeWidth="1"/>}
+					id={'header-wish'}
+					url={'/wishlist'}
+					items={wish}/>
+				<MobileHeaderButton 
+					headerIcon={<ShoppingCart size={"2rem"} strokeWidth="1"/>}
+					id={'header-cart'}
+					url={'/cart'}
+					items={cart}/>
+				<Hamburger size={'2rem'} handleClick={showMenu} expanded={expanded}/>
+			</div>
+		</div>
+	);
 }
 
 function mapStateToProps(reduxState) {
