d0001cff99d96f65da7af58a6835460c8a62c763
STM <startinmerc@gmail.com>
2020-04-16 11:39:52 +0100

    Remove unused function

--- src/Components/Products/ProductList.js
+++ src/Components/Products/ProductList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from "react-redux";
 import { fetchProducts, fetchCategoryProducts } from "../../store/actions/products";
-import { getItems } from "../../middleware";
 import ProductCard from './ProductCard';
 import './ProductList.css';
 
