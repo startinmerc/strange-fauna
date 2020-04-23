454e9ea62ca1a341b6e247940a464bcb136a3fa8
STM <startinmerc@gmail.com>
2020-04-23 12:48:33 +0100

    Handle total updating in cart handler

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -1,16 +1,21 @@
 import { ADD_CART, REMOVE_CART, EDIT_CART_QTY } from "../actionTypes";
 
 const DEFAULT_STATE = {
-	cart: []
+	cart: [],
+	total: 0
 }
 
 export default (state = DEFAULT_STATE, action) => {
 	switch(action.type){
 		case ADD_CART:
-			return {...state, cart: [...state.cart, {id: action.id, qty: action.qty}]};
+			let newAddCart = [...state.cart, {id: action.id, qty: action.qty, price: action.price}];
+			let newAddTotal = state.total + (action.price * action.qty);
+			return {cart: newAddCart, total: newAddTotal};
 		case REMOVE_CART:
-			let newCart = state.cart.filter(val => val.id !== action.id);
-			return {...state, cart: newCart};
+			let foundItem = state.cart.find(v=>(v.id === action.id));
+			let newRemoveCart = state.cart.filter(val => val.id !== action.id);
+			let newRemoveTotal = state.total - (foundItem.price * foundItem.qty);
+			return {cart: newRemoveCart, total: newRemoveTotal};
 		case EDIT_CART_QTY:
 			let editedCart = state.cart.map((item) => {
 				if (item.id === action.id) {
