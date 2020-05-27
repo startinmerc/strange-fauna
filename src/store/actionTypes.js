a2fa6e0a70e1812fbf4c8a10ad614eefa9286942
STM <startinmerc@gmail.com>
2020-05-27 18:48:29 +0100

    Add LOAD_DELIVERIES actionType

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,14 +1,22 @@
+// Wishlist
 export const ADD_WISH = "ADD_WISH";
 export const REMOVE_WISH = "REMOVE_WISH";
+
+// Cart
 export const ADD_CART = "ADD_CART";
 export const REMOVE_CART = "REMOVE_CART";
 export const EDIT_CART_QTY = "EDIT_CART_QTY";
+
+// Other / Deliveries
+export const LOAD_DELIVERIES = "LOAD_DELIVERIES";
 export const CHANGE_DELIVERY = "CHANGE_DELIVERY";
 
+// Products
 export const LOAD_PRODUCTS = "LOAD_PRODUCTS";
 export const LOAD_NAV_PRODUCTS = "LOAD_NAV_PRODUCTS";
 export const LOAD_ONE_PRODUCT = "LOAD_ONE_PRODUCT";
 export const LOAD_CATEGORY_PRODUCTS = "LOAD_CATEGORY_PRODUCTS";
 export const CLEAR_SEARCH = "CLEAR_SEARCH";
 
+// Categories
 export const LOAD_CATEGORIES = "LOAD_CATEGORIES";
