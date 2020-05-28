99d48ac6454133842e13690f9c47a4fd548161a3
STM <startinmerc@gmail.com>
2020-05-28 10:45:26 +0100

    Switch Cart to handle api

--- src/Components/Carts/Cart.js
+++ src/Components/Carts/Cart.js
@@ -1,47 +1,58 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from "react-redux";
-import { changeDelivery } from '../../store/actions/other';
+import { fetchOneProduct, clearSearch } from "../../store/actions/products";
+import { fetchDeliveries, changeDelivery } from '../../store/actions/other';
 import { Link } from 'react-router-dom';
 import ProductCard from '../Products/ProductCard';
-import { deliveries } from '../../seeds';
 import './Carts.css';
-import { getItems, getTotal } from '../../middleware';
 
 // No props expected
 // Returns cart element
 
-const Cart = ({ changeDelivery, cart, delivery })=> {
+const Cart = ({ fetchOneProduct, clearSearch, changeDelivery, cart, delivery, deliveryOptions, search, fetchDeliveries })=> {
+
+	React.useEffect(()=>{
+		fetchDeliveries();
+		clearSearch();
+		cart.cart.forEach((v)=>{
+			fetchOneProduct(v.id);
+		});
+		// ComponentWillUnmount function to empty search array
+		return function cleanUp(){clearSearch()}
+	},[cart, clearSearch, fetchOneProduct, fetchDeliveries]);
 	
 	function handleChange(e) {
 		changeDelivery(e.target.value);
 	}
-	// Get full item info
-	let items = getItems(cart);
-	let total = getTotal(items);
-		
+
+
 	return (
 		<main id="cart">
 			<Helmet>
 				<title>Strange Flora - Cart</title>
 			</Helmet>
 			<div>
-				<h2>Your Cart{(items.length === 0) ? ' is empty' : null}</h2>
+				<h2>Your Cart{(cart.cart.length === 0) ? ' is empty' : null}</h2>
 			</div>
 			<div className="cart__list">
-				{items.map(item => (
+				{search.map(item => (
 					<ProductCard detail={item} key={`prod-${item.id}`} type="cart"/>
 				))}
 			</div>
 			<div style={{textAlign: 'right'}}>
-				<p className="cart__subtotal">Subtotal: ${total}</p>
+				<p className="cart__subtotal">Subtotal: ${cart.total}</p>
 				<p>
 					<label htmlFor="deliveries">Choose delivery option:</label>
 					<select value={delivery} onChange={handleChange} id="deliveries" name="deliveries">
-						{deliveries.map((op,i) => (<option key={`del-op-${i}`} value={op.price}>{op.name} - ${op.price}</option>))}
+						{deliveryOptions.map((op,i) => (
+							<option key={`del-op-${i}`} value={op.price}>
+								{op.title} - {op.speed} day(s) - ${op.price}
+							</option>
+							))}
 					</select>
 				</p>
-				<h2>Total: ${total + Number(delivery)}</h2>
+				<h2>Total: ${cart.total + Number(delivery)}</h2>
 				<Link to="/checkout">Proceed to Checkout</Link>
 			</div>
 		</main>
@@ -50,9 +61,11 @@ const Cart = ({ changeDelivery, cart, delivery })=> {
 
 function mapStateToProps(reduxState) {
 	return {
-		cart: reduxState.cart.cart,
-		delivery: reduxState.other.delivery
+		cart: reduxState.cart,
+		delivery: reduxState.other.delivery,
+		deliveryOptions: reduxState.other.deliveryOptions,
+		search: reduxState.products.search
 	};
 }
 
-export default connect(mapStateToProps, { changeDelivery })(Cart);
+export default connect(mapStateToProps, { changeDelivery, fetchOneProduct, clearSearch, fetchDeliveries })(Cart);
