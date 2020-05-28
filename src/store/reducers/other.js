bae6ad4aea13216baf9084e4c8e6d74234e3f914
STM <startinmerc@gmail.com>
2020-05-28 10:44:35 +0100

    Add api delivery reducer & default state

--- src/store/reducers/other.js
+++ src/store/reducers/other.js
@@ -1,11 +1,14 @@
-import { CHANGE_DELIVERY } from "../actionTypes";
+import { LOAD_DELIVERIES, CHANGE_DELIVERY } from "../actionTypes";
 
 const DEFAULT_STATE = {
-	delivery: 50
+	deliveryOptions: [],
+	delivery: 0
 };
 
 export default (state = DEFAULT_STATE, action) => {
 	switch (action.type) {
+		case LOAD_DELIVERIES:
+			return {...state, deliveryOptions: action.deliveries};
 		case CHANGE_DELIVERY:
 			return {...state, delivery: Number(action.delivery)};
 		default:
