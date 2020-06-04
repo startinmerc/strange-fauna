81163ccf8840bdf2924992f4e2c5dcd4c709fa19
STM <startinmerc@gmail.com>
2020-06-04 12:05:09 +0100

    Remove unused wish

--- src/Components/Products/ProductList.js
+++ src/Components/Products/ProductList.js
@@ -5,10 +5,10 @@ import { fetchProducts, fetchCategoryProducts } from "../../store/actions/produc
 import ProductCard from './ProductCard';
 import './ProductList.css';
 
-// Expects 'type' string as prop, either all, wish or product category, and title string
+// Expects 'type' string as prop, either all or product category, and title string
 // Returns main element with responsive grid display of products
 
-const ProductList = ({ type, wish, title, fetchProducts, products, fetchCategoryProducts })=> {
+const ProductList = ({ type, title, fetchProducts, products, fetchCategoryProducts })=> {
 
 	React.useEffect(()=>{
 		function getList(type){
@@ -17,7 +17,7 @@ const ProductList = ({ type, wish, title, fetchProducts, products, fetchCategory
 				fetchProducts();
 			} else {
 				// API call to load category products to Redux state
-				fetchCategoryProducts(type);				
+				fetchCategoryProducts(type);
 			}
 		}
 		getList(type);
@@ -48,7 +48,6 @@ const ProductList = ({ type, wish, title, fetchProducts, products, fetchCategory
 
 function mapStateToProps(state){
 	return {
-		wish: state.wish.wish,
 		products: state.products.products
 	};
 };
