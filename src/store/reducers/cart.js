fa5539676c230174e9c78c274f6eac9d5bcf0bc4
STM <startinmerc@gmail.com>
2020-06-10 11:28:39 +0100

    Refactor cart reducers to use getcartTotals

--- src/store/reducers/cart.js
+++ src/store/reducers/cart.js
@@ -12,29 +12,21 @@ export default (state = DEFAULT_STATE, action) => {
 			// Create copy of state & append new item
 			let newAddCart = [...state.cart, {id: action.id, qty: action.qty, price: action.price}];
 			// Update total
-			let newAddTotal = state.total + (action.price * action.qty);
+			let newAddTotal = getCartTotals(newAddCart);
 			// Return new state
 			return {cart: newAddCart, total: newAddTotal};
 		case REMOVE_CART:
-			// Find item in cart
-			let foundItem = state.cart.find(v=>(v.id === action.id));
 			// Filter cart to exclude item
 			let newRemoveCart = state.cart.filter(val => val.id !== action.id);
-			// Subtract item x quantity from total
-			let newRemoveTotal = state.total - (foundItem.price * foundItem.qty);
+			// Update total
+			let newRemoveTotal = getCartTotals(newRemoveCart);
 			// Return new state
 			return {cart: newRemoveCart, total: newRemoveTotal};
 		case EDIT_CART_QTY:
-			// Create copy of total
-			let newEditTotal = state.total;
 			// Create copy of cart...
-			let editedCart = state.cart.map((item) => {
+			let newEditCart = state.cart.map((item) => {
 				// If item id matches...
 				if (item.id === action.id) {
-					// remove original quantity x price from copied total
-					newEditTotal -= (item.qty * item.price);
-					// add new quantity x price to copied total
-					newEditTotal += (action.qty * item.price);
 					// change quantity in copied cart
 					item.qty = action.qty;
 					return item;
@@ -43,8 +35,10 @@ export default (state = DEFAULT_STATE, action) => {
 					return item;
 				}
 			});
+			// Create copy of total
+			let newEditTotal = getCartTotals(newEditCart);
 			// Return new state
-			return {total: newEditTotal, cart: editedCart};
+			return {total: newEditTotal, cart: newEditCart};
 		default:
 			return state;
 	};
