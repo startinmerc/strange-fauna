17ad6400a2eec8b72ab60ddbbd3c90d2eef04ede
STM <startinmerc@gmail.com>
2020-06-18 17:04:32 +0100

    Expand error object

--- src/store/reducers/errors.js
+++ src/store/reducers/errors.js
@@ -1,11 +1,11 @@
 import { ADD_ERROR, REMOVE_ERROR } from "../actionTypes";
 
-export default (state = {message: null}, action) => {
+export default (state = {message: null, status: null}, action) => {
 	switch (action.type) {
 		case ADD_ERROR:
-			return { ...state, message: action.error };
+			return { ...state, message: action.error.message, status: action.error.status };
 		case REMOVE_ERROR:
-			return { ...state, message: null };
+			return { ...state, message: null, status: null };
 		default:
 			return state;
 	}
