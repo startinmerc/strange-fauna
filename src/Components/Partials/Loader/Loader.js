cebfdeb4c587e3185edb88e0f243bc79cb4adf77
STM <startinmerc@gmail.com>
2020-06-18 16:58:03 +0100

    Style Loader component as class with fullscreen variant

--- src/Components/Partials/Loader/Loader.js
+++ src/Components/Partials/Loader/Loader.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Loader.css";
 
-export default function Loader(){
+export default function Loader({fullScreen}){
 	return (
-		<div id="loader">
-			<h1>Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span></h1>
+		<div className={`loader ${fullScreen && "loader--full"}`}>
+			<h1 className="header__title">Strange Flora</h1>
+			<h2>
+				Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span>
+			</h2>
 		</div>
 	)
 }
