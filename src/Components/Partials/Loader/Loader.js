ea7457f2d4b0ec77bb624d56f7052f50dfa343d2
STM <startinmerc@gmail.com>
2020-09-14 17:31:10 +0200

    Add SVG animation to Loader

--- src/Components/Partials/Loader/Loader.js
+++ src/Components/Partials/Loader/Loader.js
@@ -4,6 +4,9 @@ import "./Loader.css";
 export default function Loader({fullScreen}){
 	return (
 		<div className={`loader ${fullScreen && "loader--full"}`}>
+			<svg>
+				<text x="50%" y="50%">SF</text>
+			</svg>
 			<h1 className="header__title">Strange Flora</h1>
 			<h2>
 				Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span>
