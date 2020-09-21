de7c76fd5ec2bc174fcc7e24bd3ad0cb62fac29f
STM <startinmerc@gmail.com>
2020-09-21 17:40:36 +0200

    Add some documentation

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,16 @@ import rootReducer from "./reducers";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
+// Import cart & wishlist from localStorage if present
 const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {};
 
 export function configureStore() {
+	// Allow Redux Dev Tools if present
 	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 	const store = createStore(
 		rootReducer,
 		persistedState,
+		// Include redux-thunk for async funcs
 		composeEnhancers(
 			applyMiddleware(thunk)
 		)
