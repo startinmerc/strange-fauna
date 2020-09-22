8b09e1c0682b8a69755104fd0d0bc50e18ca42fa
STM <startinmerc@gmail.com>
2020-09-22 11:26:43 +0200

    Wrap Loader in TransitionGroup, CSSTransition

--- src/Components/Partials/Loader/Loader.js
+++ src/Components/Partials/Loader/Loader.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./Loader.css";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 export default function Loader({fullScreen, isLoading}){
 	return (
-		isLoading && (
-			<div className={`loader ${fullScreen && "loader--full"}`}>
-				<svg>
-					<text x="50%" y="50%">SF</text>
-				</svg>
-				<h1 className="header__title">Strange Flora</h1>
-				<h2>
-					Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span>
-				</h2>
-			</div>
-		)
+		<TransitionGroup component={null}>
+			{isLoading && (
+				<CSSTransition classNames="loader" timeout={600}>
+					<div className={`loader ${fullScreen && "loader--full"}`}>
+						<svg>
+							<text x="50%" y="50%">SF</text>
+						</svg>
+						<h1 className="header__title">Strange Flora</h1>
+						<h2>
+							Loading<span className="loader__dot">.</span><span className="loader__dot">.</span><span className="loader__dot">.</span>
+						</h2>
+					</div>
+				</CSSTransition>
+			)}
+		</TransitionGroup>
 	);
 };
