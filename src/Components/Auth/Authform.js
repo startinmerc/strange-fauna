b15b265799f7d85d3a55a157aee595a224189281
STM <startinmerc@gmail.com>
2020-09-23 16:44:01 +0200

    Add conditional styles to Authform

--- src/Components/Auth/Authform.js
+++ src/Components/Auth/Authform.js
@@ -12,16 +12,28 @@ const Authform = ({type}) => {
 		updateData({...data, [e.target.name]: e.target.value})
 	}
 
+	const styles = {
+		up: {
+			header: "Sign Up",
+			submit: "Register!"
+		},
+		in: {
+			header: "Sign In",
+			submit: "Sign In!"
+		}
+	}
+
 	return (
 		<main>
 			<form>
-				<h2>{type}</h2>
-				<label htmlFor="email">Email:</label>
-				<input type="email" id="email" name="email" placeholder="your@email.com" value={data.email} onChange={handleChange}></input>
+				<h2>{styles[type].header}</h2>
+				{type === "up" && <><label htmlFor="email">Email:</label>
+				<input type="email" id="email" name="email" placeholder="your@email.com" value={data.email} onChange={handleChange}></input></>}
 				<label htmlFor="username">Username:</label>
 				<input type="text" id="username" name="username" placeholder="desired username" value={data.username} onChange={handleChange}></input>
 				<label htmlFor="password">Password:</label>
 				<input type="password" id="password" name="password" placeholder="desired password" value={data.password} onChange={handleChange}></input>
+				<button type="submit">{styles[type].submit}</button>
 			</form>
 		</main>
 	)
