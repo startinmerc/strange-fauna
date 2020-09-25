8194ce5594ea9e18307489fa3909885316979604
STM <startinmerc@gmail.com>
2020-09-25 13:05:01 +0200

    Add user to HeaderButtonContainer

--- src/Components/Partials/Header/HeaderButtonContainer.js
+++ src/Components/Partials/Header/HeaderButtonContainer.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from "react-redux";
 import Star from '../../SVGs/Star';
 import ShoppingCart from '../../SVGs/ShoppingCart';
+import User from '../../SVGs/User';
 import HeaderButton from './HeaderButton';
 
-const HeaderButtonContainer = ({wish,cart}) => {
+const HeaderButtonContainer = ({wish,cart, currentUser}) => {
 
 	return (
 		<div className="header__buttons-container">
@@ -20,6 +21,12 @@ const HeaderButtonContainer = ({wish,cart}) => {
 			 id={'header-cart'}
 			 url={'/cart'}
 			 items={cart.cart} total={cart.total}/>
+			<HeaderButton 
+			 headerIcon={<User size={'24px'} strokeWidth="2" color="var(--black)"/>}
+			 headerText={currentUser.isAuthenticated ? `${currentUser.user.username}` : "Sign In"}
+			 id={'header-user'}
+			 url={'/signin'}
+			/>
 		</div>
 	);
 }
@@ -27,7 +34,8 @@ const HeaderButtonContainer = ({wish,cart}) => {
 function mapStateToProps(reduxState) {
 	return {
 		wish: reduxState.wish,
-		cart: reduxState.cart
+		cart: reduxState.cart,
+		currentUser: reduxState.currentUser
 	};
 }
 
