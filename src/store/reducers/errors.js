1f4f49e8e9d48aed73dd808b2e95d49f31b91199
STM <startinmerc@gmail.com>
2020-09-27 17:38:36 +0200

    Refactor errors reducer

--- src/store/reducers/errors.js
+++ src/store/reducers/errors.js
@@ -1,11 +1,16 @@
 import { ADD_ERROR, REMOVE_ERROR } from "../actionTypes";
 
-export default (state = {message: null, status: null}, action) => {
+const DEFAULT_STATE = {
+	message: null,
+	status: null
+};
+
+export default (state = DEFAULT_STATE, action) => {
 	switch (action.type) {
 		case ADD_ERROR:
-			return { ...state, message: action.error.message, status: action.error.status };
+			return { ...state, ...action.error };
 		case REMOVE_ERROR:
-			return { ...state, message: null, status: null };
+			return { ...state, ...DEFAULT_STATE };
 		default:
 			return state;
 	}
