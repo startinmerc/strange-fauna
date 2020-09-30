fc6392363a023900b09d663b73ab71eeab04c53c
STM <startinmerc@gmail.com>
2020-09-30 12:40:56 +0200

    Pass fill prop to HeaderIcons

--- src/Components/Partials/Header/HeaderButtonContainer.js
+++ src/Components/Partials/Header/HeaderButtonContainer.js
@@ -1,41 +1,61 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
-import Star from '../../SVGs/Star';
-import ShoppingCart from '../../SVGs/ShoppingCart';
-import User from '../../SVGs/User';
-import HeaderButton from './HeaderButton';
+import Star from "../../SVGs/Star";
+import ShoppingCart from "../../SVGs/ShoppingCart";
+import User from "../../SVGs/User";
+import HeaderButton from "./HeaderButton";
 
-const HeaderButtonContainer = ({wish,cart, currentUser}) => {
-
+const HeaderButtonContainer = ({ wish, cart, currentUser }) => {
 	return (
 		<div className="header__buttons-container">
-			<HeaderButton 
-			 headerIcon={<Star size={'24px'} strokeWidth="2" color="var(--black)"/>}
-			 headerText={'Wishlist'}
-			 id={'header-wish'}
-			 url={'/wishlist'}
-			 items={wish}/>
-			<HeaderButton 
-			 headerIcon={<ShoppingCart size={'24px'} strokeWidth="2" color="var(--black)"/>}
-			 headerText={'Cart'}
-			 id={'header-cart'}
-			 url={'/cart'}
-			 items={cart.cart} total={cart.total}/>
-			<HeaderButton 
-			 headerIcon={<User size={'24px'} strokeWidth="2" color="var(--black)"/>}
-			 headerText={currentUser.isAuthenticated ? `${currentUser.user.username}` : "Sign In"}
-			 id={'header-user'}
-			 url={currentUser.isAuthenticated ? '/userpage' : '/signin'}
+			<HeaderButton
+				headerIcon={
+					<Star size={"24px"} strokeWidth="2" fill={!!wish.length > 0} />
+				}
+				headerText={"Wishlist"}
+				id={"header-wish"}
+				url={"/wishlist"}
+				items={wish}
+			/>
+			<HeaderButton
+				headerIcon={
+					<ShoppingCart
+						size={"24px"}
+						strokeWidth="2"
+						fill={!!cart.cart.length > 0}
+					/>
+				}
+				headerText={"Cart"}
+				id={"header-cart"}
+				url={"/cart"}
+				items={cart.cart}
+				total={cart.total}
+			/>
+			<HeaderButton
+				headerIcon={
+					<User
+						size={"24px"}
+						strokeWidth="2"
+						fill={currentUser.isAuthenticated}
+					/>
+				}
+				headerText={
+					currentUser.isAuthenticated
+						? `${currentUser.user.username}`
+						: "Sign In"
+				}
+				id={"header-user"}
+				url={currentUser.isAuthenticated ? "/userpage" : "/signin"}
 			/>
 		</div>
 	);
-}
+};
 
 function mapStateToProps(reduxState) {
 	return {
 		wish: reduxState.wish,
 		cart: reduxState.cart,
-		currentUser: reduxState.currentUser
+		currentUser: reduxState.currentUser,
 	};
 }
 
--- src/Components/SVGs/User.js
+++ src/Components/SVGs/User.js
@@ -1,14 +1,28 @@
 import React from "react";
 
-const User = ({ size = 48, color = "#000000", strokeWidth="1" }) => (
-  
-  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}
-  viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} role="img" 
-    strokeLinecap="butt" strokeLinejoin="arcs" className="icon user">
-    <title>User Icon</title>
-    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-    <circle cx="12" cy="7" r="4"></circle>
-  </svg>
+const User = ({
+	size = 48,
+	color = "var(--black)",
+	fill = false,
+	strokeWidth = "1",
+}) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={size}
+		height={size}
+		viewBox="0 0 24 24"
+		fill={fill ? "var(--primary)" : "none"}
+		stroke={color}
+		strokeWidth={strokeWidth}
+		role="img"
+		strokeLinecap="butt"
+		strokeLinejoin="arcs"
+		className="icon user"
+	>
+		<title>User Icon</title>
+		<path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+		<circle cx="12" cy="7" r="4"></circle>
+	</svg>
 );
 
 export default User;
