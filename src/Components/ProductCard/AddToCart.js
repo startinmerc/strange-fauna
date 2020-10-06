5a0859a99f6c3be17f2e2d0e977a81af0b21c022
STM <startinmerc@gmail.com>
2020-10-06 12:11:54 +0200

    AddToCart to functional component

--- src/Components/ProductCard/AddToCart.js
+++ src/Components/ProductCard/AddToCart.js
@@ -1,51 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import { addCart, removeCart } from "../../store/actions/cart";
 import { connect } from "react-redux";
 
 // Expects product id
 // Returns add to cart button with conditional formatting
 
-class AddToCart extends Component {
-	// default 1 qty for adding to cart
-	static defaultProps = {
-		qty: 1,
-		classes: "",
-	};
+function AddToCart({
+	addCart,
+	cart,
+	classes = "",
+	id,
+	price,
+	qty = 1,
+	removeCart,
+	stk,
+}) {
+	// If filtered cart by props.id is non-zero
+	const inCart = cart.filter((item) => item.id === id).length > 0;
 
-	handleClick(id, qty, inCart, price) {
+	function handleClick() {
 		if (inCart) {
 			// Take obj from cart (Redux)
-			this.props.removeCart(id);
+			removeCart(id);
 		} else {
 			// Add obj to cart (Redux)
-			this.props.addCart(id, qty, price);
+			addCart(id, qty, price);
 		}
 	}
 
-	render() {
-		// If filtered cart by props.id is non-zero
-		const inCart =
-			this.props.cart.filter((item) => item.id === this.props.id).length > 0;
-		return (
-			<button
-				className={`${inCart ? "button--added" : ""} ${this.props.classes}`}
-				disabled={this.props.stk < 1 && !inCart ? "disabled" : null}
-				onClick={this.handleClick.bind(
-					this,
-					this.props.id,
-					this.props.qty,
-					inCart,
-					this.props.price
-				)}
-			>
-				{inCart
-					? "Remove from Cart"
-					: this.props.stk > 0
-					? "Add to Cart"
-					: "Out of Stock"}
-			</button>
-		);
-	}
+	return (
+		<button
+			className={`${inCart ? "button--added" : ""} ${classes}`}
+			disabled={stk < 1 && !inCart ? "disabled" : null}
+			onClick={handleClick}
+		>
+			{inCart ? "Remove from Cart" : stk > 0 ? "Add to Cart" : "Out of Stock"}
+		</button>
+	);
 }
 
 function mapStateToProps(reduxState) {
