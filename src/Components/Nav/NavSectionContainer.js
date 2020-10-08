9c6638544af8b5456007965fbe0ed943ebb2d206
STM <startinmerc@gmail.com>
2020-10-08 16:03:49 +0200

    Remove fetchCategories from NavSectionContainer

--- src/Components/Nav/NavSectionContainer.js
+++ src/Components/Nav/NavSectionContainer.js
@@ -2,25 +2,10 @@ import React from "react";
 import "./NavSectionContainer.css";
 import NavSection from "./NavSection";
 import { connect } from "react-redux";
-import { fetchNavProducts } from "../../store/actions/products";
 
 // Returns element containing nav sections with hover menus
 
-function NavSectionContainer({ categories, products, fetchNavProducts }) {
-	React.useEffect(() => {
-		// If there are no NavProducts and categories have been loaded
-		if (products.length === 0 && categories.length !== 0) {
-			// For each category
-			categories.forEach((cat) => {
-				// As long as category isn't about
-				if (cat.type !== "about") {
-					// Send GET request to get category_id's featured products
-					fetchNavProducts(cat._id);
-				}
-			});
-		}
-	}, [categories, products, fetchNavProducts]);
-
+function NavSectionContainer({ categories, products }) {
 	// Map categories to NavSections
 	const sections = categories.map((cat) => (
 		<NavSection
@@ -45,6 +30,4 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps, { fetchNavProducts })(
-	NavSectionContainer
-);
+export default connect(mapStateToProps)(NavSectionContainer);
